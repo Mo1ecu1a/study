b2a8818f6539cdd777430ae4108bd784ac6d1e60
Mo1ecu1a <puazhue@gmail.com>
2026-05-17 21:07:07 +0300

    class go & map

--- mario_game_oop/main.cpp
+++ mario_game_oop/main.cpp
@@ -8,6 +8,39 @@
 #include <cmath> 
 
 
+class GameObject {
+    public:
+        float x, y;
+        float width, height;
+        float vertSpeed;
+        bool IsFly;
+        char cType;
+        float horizSpeed;
+
+        GameObject *get_new_brick(GameObject *&brick, int &brickLength) {
+            brickLength++;
+            brick = (GameObject*)realloc(brick, sizeof(*brick) * brickLength);
+            return brick + brickLength - 1;
+        }
+        GameObject *get_new_moving(GameObject *&moving, int &movingLength) {
+            movingLength++;
+            moving = (GameObject*)realloc(moving, sizeof(*moving) * movingLength);
+            return moving + movingLength - 1;
+        }
+        void init_object(GameObject *obj, float xPos, float yPos, float oWidth, float oHeigth, char inType) {
+            set_object_position(obj, xPos, yPos);
+            width = oWidth;
+            height = oHeigth;
+            vertSpeed = 0;
+            cType = inType;
+            horizSpeed = 0.2;
+        }
+        void set_object_position(GameObject *obj, float xPos, float yPos) {
+            x = xPos;
+            y = yPos;
+        }
+};
+
 
 class Map {
     private:
@@ -28,7 +61,33 @@ class Map {
             for (int j = 0; j < MAP_HIGHT; j++) {
                 mvprintw(j, 0, "%s", map[j]);
             }
-        }       
+        }   
+        void put_object_on_map(const GameObject &obj) {
+            int ix = (int)round(obj.x);
+            int iy = (int)round(obj.y);
+            int iwidth = (int)round(obj.width);
+            int iheight = (int)round(obj.height);
+
+            for (int i = ix; i < (ix + iwidth); i++) {
+                for (int j = iy; j < (iy + iheight); j++) {
+                    if (is_position_on_map(i, j))
+                        map[j][i] = obj.cType;
+                }
+            }
+        }    
+        void put_score_on_map(int &score) {
+            char c[30];
+            sprintf(c, "Score: %d", score);
+            int len = strlen(c);
+            for (int i = 0; i < len; i++) {
+                map[1][i+5] = c[i];
+            }
+        }
+        bool is_position_on_map(int x, int y) {
+            return ((x >= 0) && (x < MAP_WIDTH) 
+                    && (y >= 0) && (y < MAP_HIGHT));
+        }
+
 
 };
 
